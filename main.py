795c8198f483a031187d7a948b8c24d2a1977a04
Zukach1201 <sm.poveda@uniandes.edu.co>
2026-05-14 12:12:43 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,50 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pymongo import MongoClient
+from datetime import datetime
+import os
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+
+client = MongoClient(os.environ["MONGO_URI"])  
+# Reemplaza "MONGO_URI" con el nombre de la variable de entorno que contiene tu cadena de conexión a MongoDB
+db = client["nombre_de_tu_base_de_datos"] 
+# Reemplaza "nombre_de_tu_base_de_datos" con el nombre de tu base de datos en MongoDB
+
+
+@app.get("/")
+def inicio():
+    return {"estado": "API funcionando correctamente"}
+
+@app.get('/bares/{bar_id}/comentarios')
+def get_comentarios(bar_id: int):
+    comentarios = db["Bares"].find_one({'_id': bar_id},{'comentarios':1,"_id":0})
+    return comentarios['comentarios'] if comentarios and 'comentarios' in comentarios else []
+
+@app.post('/bares/{bar_id}/comentarios')
+def post_comentario(bar_id: int, datos: dict):
+    # datos['bar_id'] = bar_id // No es necesario agregar bar_id al comentario, ya que se almacenará dentro del documento del bar correspondiente
+    datos['fecha']  = datetime.now().isoformat()
+    db["Bares"].update_one({'_id': bar_id}, {'$push': {'comentarios': datos}})
+    return {'mensaje': 'Comentario guardado'}
+
+@app.get('/bares/{bar_id}/eventos')
+def get_eventos(bar_id: int):
+    eventos = db["Bares"].find_one({'_id': bar_id}, {'eventos': 1,"_id": 0})
+    return eventos['eventos'] if eventos and 'eventos' in eventos else []
+
+@app.post('/bares/{bar_id}/eventos')
+def post_evento(bar_id: int, datos: dict): 
+    # datos['bar_id'] = bar_id // No es necesario agregar bar_id al evento, ya que se almacenará dentro del documento del bar correspondiente
+    datos['fecha_creacion']  = datetime.now().isoformat()
+    db["Bares"].update_one({'_id': bar_id}, {'$push': {'eventos': datos}})
+    return {'mensaje': 'Evento guardado'}
+
